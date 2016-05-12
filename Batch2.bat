4e079a914ceb3318a0ced4fb35150c695f52615e
Jwarners <jacob.david.warners@gmail.com>
2016-05-12 00:22:28 -0700

    Update Batch2.bat

--- Batch2.bat
+++ Batch2.bat
@@ -1,3 +1,6 @@
 @echo off
 
-Copy /A C:\Users\%Username%\Desktop\file.txt /B C:\Users\Mistweaver\Documents
+Copy /A C:\Users\%Username%\Desktop\file.txt /B C:\Users\%Username%\Documents
+
+
+#This one simply copies the text that was made in bat1 and puts it into documents
