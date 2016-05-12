ddeeb985ab05727821b08b66b2bcdc6b413e87fb
Jwarners <jacob.david.warners@gmail.com>
2016-05-12 00:21:29 -0700

    Update Batch1.bat

--- Batch1.bat
+++ Batch1.bat
@@ -1,4 +1,4 @@
 @echo off
-
+#The purpose of this is to write a text file and the second line adds to the file that exists. Using > instead of >> will overwrite
 echo This is what it wants me to type>>C:\Users\%Username%\Desktop\file.txt
 echo Thanks to my penis>>C:\Users\%Username%\Desktop\file.txt
