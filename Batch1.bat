9ed99f1500ac0694539272971b0ccbba50c9eade
Jwarners <jacob.david.warners@gmail.com>
2016-05-12 00:20:19 -0700

    Add files via upload

---
+++ Batch1.bat
@@ -0,0 +1,4 @@
+@echo off
+
+echo This is what it wants me to type>>C:\Users\%Username%\Desktop\file.txt
+echo Thanks to my penis>>C:\Users\%Username%\Desktop\file.txt
---
+++ Batch2.bat
@@ -0,0 +1,3 @@
+@echo off
+
+Copy /A C:\Users\%Username%\Desktop\file.txt /B C:\Users\Mistweaver\Documents
